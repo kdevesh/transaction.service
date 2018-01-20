bcf8fdec57cf852d7f80a211fb14207884ccb2b6
kdevesh <devesh.juit@gmail.com>
2018-01-20 14:50:38 +0530

    Touch

--- src/main/scala/com/transaction/api/common/utils/DataStore.scala
+++ src/main/scala/com/transaction/api/common/utils/DataStore.scala
@@ -3,5 +3,5 @@ package com.transaction.api.common.utils
 import com.transaction.api.models.Transaction
 
 object DataStore {
-  var tx_list = List[Transaction]()
+  var tx_list = List[Transaction]()  //It maintains the list of all the transactions inserted in-memory.
 }
